088a7f945da8fdd7af2cc6d97fd52fb337a0181a
SperanskyArtyom <awesomepinkieguy@gmail.com>
2025-03-03 00:42:37 +0700

    create loadData module

---
+++ loadData.js
@@ -0,0 +1,15 @@
+async function loadData() {
+    let data = [];
+    let url = "https://catfact.ninja/breeds";
+    do{
+    let response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('HTTP error! status: ${response.status}'); // Обработка ошибок
+    }
+    let json = await response.json();
+    for (let item of json["data"])
+        data.push(item);
+    url = json["next_page_url"];
+    } while (url);
+    return data;
+}
